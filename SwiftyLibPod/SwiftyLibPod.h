c0b376816ca9005b8454ec9fab4327d380429ca1
ibalborde <maximiliano_009@hotmail.com>
2023-01-11 22:06:56 -0300

    Initial project setup

---
+++ SwiftyLibPod/SwiftyLibPod.h
@@ -0,0 +1,18 @@
+//
+//  SwiftyLibPod.h
+//  SwiftyLibPod
+//
+//  Created by Maximiliano Ibalborde on 10/01/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftyLibPod.
+FOUNDATION_EXPORT double SwiftyLibPodVersionNumber;
+
+//! Project version string for SwiftyLibPod.
+FOUNDATION_EXPORT const unsigned char SwiftyLibPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyLibPod/PublicHeader.h>
+
+
---
+++ SwiftyLibPodExamples/SwiftyLibPodExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyLibPodExamplesApp.swift
+//  SwiftyLibPodExamples
+//
+//  Created by Maximiliano Ibalborde on 10/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftyLibPodExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
