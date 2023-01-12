e9de54f3043de5bf49b3e927f1693e9a36a9ba32
ibalborde <maximiliano_009@hotmail.com>
2023-01-11 22:57:01 -0300

    Added test cases

---
+++ SwiftyLibPod/SwiftyLibPod.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftyLibPod.swift
+//  SwiftyLibPod
+//
+//  Created by Maximiliano Ibalborde on 11/01/2023.
+//
+
+import Foundation
+
+public final class SwiftyLibPod {
+    let name = "Swifty Lib Pod"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+}
--- SwiftyLibPodTests/SwiftyLibPodTests.swift
+++ SwiftyLibPodTests/SwiftyLibPodTests.swift
@@ -9,28 +9,17 @@ import XCTest
 @testable import SwiftyLibPod
 
 final class SwiftyLibPodTests: XCTestCase {
+    var swiftyLibPod: SwiftyLibPod!
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    override func setUp() {
+        swiftyLibPod = SwiftyLibPod()
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func testAdd() {
+        XCTAssertEqual(swiftyLibPod.add(a: 1, b: 1), 2)
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    func testSub() {
+        XCTAssertEqual(swiftyLibPod.sub(a: 2, b: 1), 1)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
